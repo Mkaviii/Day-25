04c618dfc279b9de859be1e608b89f5dcc01b59a
Mkaviii <mkaviii49@gmail.com>
2025-07-22 23:49:14 +0530

    react fundamentals

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import Greeting from './Greeting';
+
+// /*
+// <></>  -> fragment tag.
+// */
+
+
+//parent components
+
+function App() {
+  const loggedInuser = "kavitha";
+  return (
+   <>
+    <Greeting loggedInuser={loggedInuser}/>
+    <Greeting/>
+    
+   </>
+  )
+}
+
+export default App;
+
